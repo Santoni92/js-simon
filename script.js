1f7599cec0d1caa90c5ab3f3b41fd1107251d3f5
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-18 17:49:46 +0100

    create delle funzioni cosi da snellire il codice

--- script.js
+++ script.js
@@ -2,33 +2,18 @@ console.log('JS OK!');
 
 //Visualizzare in pagina (o in console) 5 numeri casuali
 
-const arrayNumeriDaIndovinare = []; //array in cui memorizzare i numeri generati random e che l'utente dovrà indovinare
-for(let i = 0; i < 5; i++)
-{
-    console.log(Math.floor(Math.random() * 10) + 1);    //stampo in console 5 numeri generati random nel range [1,10] che sarebbero i numeri da indovinare da parte dell'utente
-    arrayNumeriDaIndovinare.push(Math.floor(Math.random() * 10) + 1);
-}
+const arrayNumeriDaIndovinare = creaArrayNumeriDaIndovinare(); //array in cui memorizzare i numeri generati random e che l'utente dovrà indovinare
 
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 setTimeout(inserisciNumero,3000);  //dopo i 30 secondi richiamo la funzione per far inserire da parte dell'utente un numero
 
 
 
+/***********************************************implementazione funzioni **********************************************/
+
 function inserisciNumero()
 {
-    const arrayNumeriUtente = [];   //inizializzo un array vuoto che sarà destinato a contenere i cinque numeri inseriti dall'utente
-    for(let i = 0; i < 5; i++)
-    {
-        let numeroInseritoDallUtente = parseInt(prompt('Inserisci numero: '));
-        while(isNaN(numeroInseritoDallUtente))  //rifaccio la richiesta di inserimento numero da parte dell'utente finché quest'ultimo non inserisce un numero
-        {
-            numeroInseritoDallUtente = parseInt(prompt('Inserisci numero: '));
-        }
-        arrayNumeriUtente.push(numeroInseritoDallUtente);   //inserisco il numero dato dall'utente nell'array
-    }
-    console.log(arrayNumeriUtente); //stampo in console l'array dei cinque numeri forniti dall'utente
-
-    
+    const arrayNumeriUtente = creaArrayNumeriUtente();   //inizializzo l'array che sarà destinato a contenere i cinque numeri inseriti dall'utente
     let numeriIndovinatiDallUtente = 0;
     const arrayNumeriIndovinati = [];   //inserirò gli eventuali numeri indovinati dall'utente
     for(let i = 0; i < arrayNumeriDaIndovinare.length; i++)   //ciclo per tutta la lunghezza dell'array dei numeri inseriti
@@ -51,8 +36,37 @@ function inserisciNumero()
         console.log('I numeri da te indovinati sono' + " " + arrayNumeriIndovinati);
     }
 }
-/*
-CreaArrayNumeriDaIndovianre()
-{
 
-}*/
+function creaArrayNumeriDaIndovinare()
+{
+    const arrayNumeriDaIndovinare = [];
+    for(let i = 0; i < 5; i++)
+    {
+        console.log(Math.floor(Math.random() * 10) + 1);    //stampo in console 5 numeri generati random nel range [1,10] che sarebbero i numeri da indovinare da parte dell'utente 
+        arrayNumeriDaIndovinare.push(Math.floor(Math.random() * 10) + 1);
+    }
+   return arrayNumeriDaIndovinare;
+}
+
+function creaArrayNumeriUtente()
+{
+    const arrayNumeriUtente = [];
+    for(let i = 0; i < 5; i++)
+    {
+        let numeroInseritoDallUtente = parseInt(prompt('Inserisci numero: '));
+        numeroInseritoDallUtente = controllaNumeroInserito(numeroInseritoDallUtente);
+        arrayNumeriUtente.push(numeroInseritoDallUtente);   //inserisco il numero dato dall'utente nell'array
+    }
+    console.log(arrayNumeriUtente); //stampo in console l'array dei cinque numeri forniti dall'utente
+    return arrayNumeriUtente;
+}
+
+function controllaNumeroInserito(numeroInserito)
+{
+    while(isNaN(numeroInserito))  //rifaccio la richiesta di inserimento numero da parte dell'utente finché quest'ultimo non inserisce un numero
+        
+    {
+        numeroInserito = parseInt(prompt('Inserisci numero: '));
+    }
+    return numeroInserito
+}
