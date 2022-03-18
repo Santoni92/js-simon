bdec793ab5785380d3f31e317e6867ac542133fd
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-18 16:15:46 +0100

    aggiunto array in cui memorizzare i numeri random che l'utente dovrà poi indovinare

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ console.log('JS OK!');
 
 //Visualizzare in pagina (o in console) 5 numeri casuali
 
+const arrayNumeriDaIndovinare = []; //array in cui memorizzare i numeri generati random e che l'utente dovrà indovinare
 for(let i = 0; i < 5; i++)
 {
-    console.log(Math.floor(Math.random() * 10) + 1);    //stampo in console 5 numeri generati random nel range [1,10]
+    console.log(Math.floor(Math.random() * 10) + 1);    //stampo in console 5 numeri generati random nel range [1,10] che sarebbero i numeri da indovinare da parte dell'utente
+    arrayNumeriDaIndovinare.push(Math.floor(Math.random() * 10) + 1);
 }
 
 //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
@@ -12,7 +14,7 @@ setTimeout(inserisciNumero,30000);  //dopo i 30 secondi richiamo la funzione per
 
 function inserisciNumero()
 {
-    const arrayNumeriUtente = [];   //inizializzo un array vuuoto che sarà destinato a contenere i cinque numeri inseriti dall'utente
+    const arrayNumeriUtente = [];   //inizializzo un array vuoto che sarà destinato a contenere i cinque numeri inseriti dall'utente
     for(let i = 0; i < 5; i++)
     {
         let numeroInseritoDallUtente = parseInt(prompt('Inserisci numero: '));
@@ -23,5 +25,11 @@ function inserisciNumero()
         arrayNumeriUtente.push(numeroInseritoDallUtente);   //inserisco il numero dato dall'utente nell'array
     }
     console.log(arrayNumeriUtente); //stampo in console l'array dei cinque numeri forniti dall'utente
-    
+
+    //Dopo che sono stati inseriti i 5 numeri, il software dice quanti dei numeri da indovinare sono stati individuati
+    let numeriIndovinatiDallUtente = 0;
+    /*for(let i = 0; i < arrayNumeriUtente.length; i++)
+    {
+        if(arrayNumeriUtente [i] == )
+    }*/
 }
